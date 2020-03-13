42fbf5520038c25e84d5c77e5d127e8861a63c4a
NMaslenko <nvmaslenko@gmail.com>
2020-03-13 18:43:54 +0500

    -- from main page relocated two methods: for jump to login page, and for check login link

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,9 +4,11 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from math import sin, log
+from .locators import BasePageLocators
 
 
 class BasePage():
+
 	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
@@ -52,3 +54,10 @@ class BasePage():
 			return False
 
 		return True
+
+	def go_to_login_page(self):
+		login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+		login_link.click()
+
+	def should_be_login_link(self):
+		assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
