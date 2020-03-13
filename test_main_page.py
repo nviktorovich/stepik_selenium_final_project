fdc247a6cbc7b387f0bc47ad48af84ab31bd7ea0
NMaslenko <nvmaslenko@gmail.com>
2020-03-13 23:07:05 +0500

    unite login tests to TestLoginFromMainPage class

--- test_main_page.py
+++ test_main_page.py
@@ -2,22 +2,28 @@ from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.locators import LoginPageLocators
 from .pages.basket_page import BasketPage
+import pytest
 
 
-def test_guest_can_go_to_login_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209"
-	page = BasePage(browser, link)
-	page.open()
-	page.go_to_login_page()
-	login_page = LoginPage(browser, browser.current_url)
-	login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+	def test_guest_can_go_to_login_page(self, browser):
+		link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209"
+		page = BasePage(browser, link)
+		page.open()
+		page.go_to_login_page()
+		login_page = LoginPage(browser, browser.current_url)
+		login_page.should_be_login_page()
+
+	def test_guest_should_see_login_link(self,browser):
+		link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209"
+		page = BasePage(browser, link)
+		page.open()
+		page.should_be_login_link()
 
 
-def test_guest_should_see_login_link(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209"
-	page = BasePage(browser, link)
-	page.open()
-	page.should_be_login_link()
+
 
 
 def test_guest_should_see_correct_login_url(browser):
