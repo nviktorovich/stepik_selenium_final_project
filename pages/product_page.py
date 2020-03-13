f0b8085e219d7f3194446d60430268ffc54251c4
NMaslenko <nvmaslenko@gmail.com>
2020-03-13 12:35:24 +0500

    -- separate all tests to different functions -- add checking absent of success message on the page -- add checking disappeared of success message from the page

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-import time
 
 class ProductPage(BasePage):
 
@@ -9,11 +8,20 @@ class ProductPage(BasePage):
 		add_to_cart_button.click()
 		self.solve_quiz_and_get_code()
 
+	def should_the_same_titles_in_cart_and_added_product(self):
 		added_product = self.browser.find_element(*ProductPageLocators.ADD_PRODUCT_TITLE).text
 		product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+		assert added_product == product_name, f'different between product:"{product_name}" and "{added_product}"'
+
+	def should_the_same_prices_in_cart_and_added_product(self):
 		total_price = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE).text
 		product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-
-		assert added_product == product_name, f'different between product:"{product_name}" and "{added_product}"'
 		assert total_price == product_price, f'different between prices "{total_price} and {product_price}"'
-		#time.sleep(2)
+
+	def should_not_be_success_message(self):
+		assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+			"Success message is presented, but should not be"
+
+	def should_dissapear_of_success_message(self):
+		assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+		    "Success message is not disappeared"
