1a3e4804652df97a8d8253186d8b572c2738ffee
NMaslenko <nvmaslenko@gmail.com>
2020-03-13 21:16:54 +0500

    -- add jump to basket page method

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,14 +5,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from math import sin, log
 from .locators import BasePageLocators
+from .locators import BasketPageLocators
+import time
 
 
-class BasePage():
+class BasePage:
 
 	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
-		#self.browser.implicitly_wait(timeout)
+		self.browser.implicitly_wait(timeout)
 
 	def open(self):
 		self.browser.get(self.url)
@@ -61,3 +63,8 @@ class BasePage():
 
 	def should_be_login_link(self):
 		assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+	def go_to_basket_page(self):
+		cart_link = self.browser.find_element(*BasketPageLocators.BASKET_LINK)
+		cart_link.click()
+
